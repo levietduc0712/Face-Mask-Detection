990ed34fb169992b779da349d8b39a043ae330c4
Le Viet Duc <levietduc0712@gmail.com>
2022-12-16 09:29:04 +0700

    first commit

--- main.py
+++ main.py
@@ -6,19 +6,27 @@ from scipy.spatial import distance
 
 model = load_model('masknet2.h5')
 face_model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-mask_label = {0:'MASK',1:'NO MASK'}
-dist_label = {0:(0,255,0),1:(255,0,0)}
+mask_label = {0: 'MASK', 1: 'NO MASK'}
+dist_label = {0: (0, 255, 0), 1: (255, 0, 0)}
 
 MIN_DISTANCE = 230
+path = 'test.jpg'
 
 #trying it out on a sample image
-img = cv2.VideoCapture(0)
+img = cv2.imread(path)
 
 img = cv2.cvtColor(img, cv2.IMREAD_GRAYSCALE)
 
-faces = face_model.detectMultiScale(img,scaleFactor=1.1,minNeighbors=3,minSize=(30, 30)) #lbpcascade
+#faces = face_model.detectMultiScale(img,scaleFactor=1.1,minNeighbors=5,minSize=(20, 20)) #haarcascade
+faces = face_model.detectMultiScale(img,scaleFactor=1.1,minNeighbors=5,minSize=(30, 30)) #lbpcascade
 
 out_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #colored output image
+
+sample_mask_img = cv2.imread(path)
+sample_mask_img = cv2.resize(sample_mask_img,(128,128))
+plt.imshow(sample_mask_img)
+sample_mask_img = np.reshape(sample_mask_img,[1,128,128,3])
+sample_mask_img = sample_mask_img/255.0
 
 #plotting
 for (x,y,w,h) in faces:
@@ -44,27 +52,6 @@ if len(faces)>=1:
         mask_result = model.predict(crop)
         cv2.putText(new_img,mask_label[mask_result.argmax()],(x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,dist_label[label[i]],2)
         cv2.rectangle(new_img,(x,y),(x+w,y+h),dist_label[label[i]],1)
-    plt.figure(figsize=(10,10))
-    plt.imshow(new_img)
-    plt.show()
-else:
-    print("No faces detected")
-
-if len(faces)>=1:
-    label = [0 for i in range(len(faces))]
-    for i in range(len(faces)-1):
-        for j in range(i+1, len(faces)):
-            dist = distance.euclidean(faces[i][:2],faces[j][:2])
-            if dist<MIN_DISTANCE:
-                label[i] = 1
-                label[j] = 1
-    new_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #colored output image
-    for i in range(len(faces)):
-        (x,y,w,h) = faces[i]
-        if label[i]==1:
-            cv2.rectangle(new_img,(x,y),(x+w,y+h),(255,0,0),1)
-        else:
-            cv2.rectangle(new_img,(x,y),(x+w,y+h),(0,255,0),1)
     plt.figure(figsize=(10,10))
     plt.imshow(new_img)
     plt.show()
